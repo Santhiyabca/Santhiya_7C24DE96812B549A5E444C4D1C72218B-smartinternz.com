85509219108b000d712d19a0b338c2ab306ab497
Santhiyabca <144257069+Santhiyabca@users.noreply.github.com>
2023-09-25 13:16:28 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,42 +1,22 @@
+class Student:
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
 
 
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-        self.__account_number = account_number
-        self.__account_holder_name = account_holder_name
-        self.__account_balance = initial_balance
+def sort_students(student_list):
+  sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+  return sorted_students
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
-        else:
-            print("Invalid deposit amount. Amount must be greater than 0.")
+students = [
+  Student("Priya", "A123", 7.8),
+  Student("Ramya", "A124", 8.9),
+  Student("Kamal", "A125", 9.1),
+  Student("Gokul", "A126", 9.9),  
+]
 
-    def withdraw(self, amount):
-        if amount > 0 and amount <= self.__account_balance:
-            self.__account_balance -= amount
-            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
-        else:
-            print("Invalid withdrawal amount or insufficient balance.")
+sorted_students = sort_students(students)
 
-    def display_balance(self):
-        print(f"Account Balance for {self.__account_holder_name}: ${self.__account_balance}")
-
-
-# Test the BankAccount class
-if __name__ == "__main__":
-    # Create an instance of BankAccount
-    my_account = BankAccount("12345", "John Doe", 1000.0)
-
-    # Display initial balance
-    my_account.display_balance()
-
-    # Deposit money
-    my_account.deposit(500)
-
-    # Withdraw money
-    my_account.withdraw(200)
-
-    # Display updated balance
-    my_account.display_balance()
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name, student.roll_number, student.cgpa))
